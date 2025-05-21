7a502d88183e9eb578d304870dbbf5d33af20ed6
Amogh Jambekar <amoghjambekar45@gmail.com>
2025-05-21 19:47:20 -0400

    FEAT: add navbar hyperlink bases and example placeholder text to index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: 'class', // enables dark mode via class strategy
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+    './components/**/*.{astro,html,js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
