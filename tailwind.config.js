35b65de28080241c8407c575fb097e495d875f88
Amogh Jambekar <amoghjambekar45@gmail.com>
2025-05-21 20:11:10 -0400

    FIX: Style update for fonts, headers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // enables dark mode via class strategy
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
-    './components/**/*.{astro,html,js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
